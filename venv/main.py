03ac8ecda4ce4c769f1ca11af4986be423cdc41d
Shardool Pathak <shardoolpathak@gmail.com>
2018-08-05 17:41:58 -0700

    Fully working, 81% accuracy with test data

--- venv/main.py
+++ venv/main.py
@@ -15,30 +15,31 @@ dataindex = range(22)
 labelindex = range(22, 32)
 training_labels = data[:5600, :, labelindex]
 training_data = data[:5600, :, dataindex]
-
+test_data = data[5605:5877, :, dataindex]
+test_label = data[5605:5877, :, labelindex]
 training_data.shape = [5600,700,22]
+print(test_label.shape, " ", test_data.shape)
+test_label = tf.reshape(test_label,[272,700,10])
+test_data = tf.reshape(test_data,[272,700,22])
 
 
 #x = tf.placeholder(tf.float32, shape=[None,training_data.shape[1],training_data.shape[2]])
 #y = tf.placeholder(tf.float32, shape=[None,training_labels.shape[1],training_labels.shape[2]])
 
 output_dim=training_labels.shape[2]
+num_nodes = 100
 
-a  = tfe.Variable(tf.random_normal([batch_size,output_dim]))
-wa = tfe.Variable(tf.random_normal([data.shape[1], output_dim, a.shape[1].value]))
-ba = tfe.Variable(tf.random_normal([data.shape[1], wa.shape[1].value, 1]))
-wx = tfe.Variable(tf.random_normal([data.shape[1], output_dim, training_data.shape[2]]))
-bx = tfe.Variable(tf.random_normal([data.shape[1], wx.shape[1].value, 1]))
-wy = tfe.Variable(tf.random_normal([data.shape[1], batch_size, batch_size]))
-by = tfe.Variable(tf.random_normal([data.shape[1], wy.shape[1].value, 1]))
+wa = tfe.Variable(tf.random_normal([data.shape[1], num_nodes, num_nodes]))
+ba = tfe.Variable(tf.random_normal([data.shape[1], 1, wa.shape[1].value]))
+wx = tfe.Variable(tf.random_normal([data.shape[1], num_nodes, training_data.shape[2]]))
+bx = tfe.Variable(tf.random_normal([data.shape[1], 1, wx.shape[1].value]))
+wy = tfe.Variable(tf.random_normal([data.shape[1], output_dim, num_nodes]))
+by = tfe.Variable(tf.random_normal([data.shape[1], 1, output_dim]))
 
 def train(x, y):
     preds = forward_prop(x)
-    preds = tf.convert_to_tensor(preds,dtype=tf.float32)
-    #print(preds.dtype)
-    preds = tf.transpose(preds,[1,0,2])
    # print(preds.shape)
-   # print(y.shape)
+    print("preds", preds.shape)
     cost = tf.reduce_mean(
         tf.reduce_sum(
             tf.subtract(
@@ -54,44 +55,48 @@ def train(x, y):
     print(cost)
     return cost
 
-def timestep(data, stepindex=0, nexta = a):
+def timestep(data, a, stepindex=0, first=False):
+    if(first):
+        a = tfe.Variable(tf.zeros([data.shape[0], num_nodes]))
+
     a1 = [tf.sigmoid(
         tf.add(
-            tf.matmul(wa[stepindex],
-                      tf.transpose(a)),
+            tf.matmul(a, tf.transpose(wa[stepindex])),
             ba[stepindex])),
         tf.sigmoid(
             tf.add(
-                tf.matmul(wx[stepindex],
-                          tf.transpose(data)),
+                tf.matmul(data,
+                          tf.transpose(wx[stepindex])),
                 bx[stepindex]))]
 
     conc = tf.add(a1[0],a1[1])
     #print(conc.shape)
 
-    yh = tf.nn.softmax(tf.add(tf.matmul(wy[stepindex], tf.transpose(conc)), by[stepindex]))
-    #print(yh.shape)
-    return tf.transpose(conc), yh
+    yh = tf.nn.softmax(tf.add((tf.matmul(conc, tf.transpose(wy[stepindex]))), by[stepindex]))
+    #print("y HAT: ",yh.shape)
+    return conc, yh
 
 
 def forward_prop(data):
     preds = []#tfe.Variable([], dtype=tf.float32) #tfe.Variable(dtype=tf.float32, graph=None)
     first = True
-    curr_a,curr_y = timestep(tf.transpose(data,[1,0,2])[0])
+    curr_a,curr_y = timestep(tf.transpose(data,[1,0,2])[0],None,first=True)
     preds.append(curr_y)
     #return tf.map_fn(lambda x: timestep(x),tf.transpose(data,[1,0,2])[1:])
     for i in tf.transpose(data,[1,0,2]):
         if first:
             first = False
         else:
-            curr_a,curr_y = timestep(i,nexta=curr_a)
+            curr_a,curr_y = timestep(i,curr_a)
             preds.append(curr_y)
+    preds = tf.convert_to_tensor(preds, dtype=tf.float32)
+    preds = tf.transpose(preds, [1, 0, 2])
     return preds
 
 
 
 
-optimizer = tf.train.AdamOptimizer(learning_rate = 1e-3)
+optimizer = tf.train.AdamOptimizer(learning_rate = 1e-2)
 for i in range(0, training_labels.shape[0], batch_size):
     train_batch = training_data[i:i + batch_size, :, :]
     label_batch = training_labels[i:i + batch_size, :, :]
@@ -100,91 +105,16 @@ for i in range(0, training_labels.shape[0], batch_size):
             tf.reshape(label_batch,
             [label_batch.shape[0],label_batch.shape[1],
            label_batch.shape[2]])))
+preds = forward_prop(test_data)
+c = 0.0
+t = 0.0
+for x in range(preds.shape[0].value):
+    for y in range(preds.shape[1].value):
+        if(np.argmax(preds[x,y,:]) == np.argmax(test_label[x,y,:])):
+            t+=1
+    t=float(t)/float(preds.shape[1].value)
+    c+=t
+acc = float(c)/float(preds.shape[0].value)
+print(acc)
 
 
-
-
-"""
-with tf.Session() as sess:
-   # sess.run(tf.contrib.eager.global_variables_initializer())
-    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
-    for i in range(0, training_labels.shape[0], batch_size):
-        print("training label shape: ", training_labels.shape)
-        print("shape of fed data",training_data[i,i+batch_size].shape)
-        train_batch = training_data[i:i+batch_size,:,:]
-        label_batch = training_labels[i:i+batch_size,:,:]
-        opt, preds = sess.run(train(tf.reshape(train_batch,
-            [train_batch.shape[0],train_batch.shape[1],train_batch.shape[2]]),
-            tf.reshape(label_batch,
-            [label_batch.shape[0],label_batch.shape[1],
-           label_batch.shape[2]])))"""
-
-
-
-
-"""
-training_labels = data[:5600, :, labelindex]
-val_data = data[5600:5877, :, dataindex]
-val_labels = data[5600:5877, :, labelindex]
-training_data = np.concatenate((training_data, val_data), axis=0)
-training_labels = np.concatenate((training_labels, val_labels), axis=0)
-x = tf.placeholder(tf.float32, shape = [training_data.shape[0],training_data.shape[1],training_data.shape[2],1])
-y = tf.placeholder(tf.float32, shape = [training_labels.shape[0],training_labels.shape[1],training_labels.shape[2],1])
-test_data = data[5877:, :, dataindex]
-test_label = data[5877:, :, labelindex]
-
-conv1 = {"weights": tf.Variable(tf.random_normal([5,5,1,50])),
-          "biases": tf.Variable(tf.random_normal([50]))}
-
-conv2 = {"weights": tf.Variable(tf.random_normal([10,10,50,60])),
-          "biases": tf.Variable(tf.random_normal([60]))}
-
-conv3 = {"weights": tf.Variable(tf.random_normal([20,20,60,80])),
-          "biases": tf.Variable(tf.random_normal([80]))}
-
-
-
-
-def forward_prop(data):
-    calc1 = tf.add(tf.nn.conv2d(data,conv1["weights"],strides=[1,1,1,1],padding="SAME"),conv1["biases"])
-    pool1 = tf.nn.max_pool(calc1,ksize=[1,4,4,1],strides=[1,2,2,1],padding="SAME")
-    calc2 = tf.add(tf.nn.conv2d(pool1,conv2["weights"],strides=[1,1,1,1],padding="SAME"),conv2["biases"])
-    pool2 = tf.nn.max_pool(calc2,ksize=[1,4,4,1],strides=[1,2,2,1],padding="SAME")
-    calc3 = tf.add(tf.nn.conv2d(pool2,conv3["weights"],strides=[1,1,1,1],padding="SAME"),conv3["biases"])
-    calc3 = tf.reshape(calc3,tf.TensorShape([calc3.shape[0].value,calc3.shape[1].value * calc3.shape[2].value * calc3.shape[3].value]))
-
-    dense1 = {"weights": tf.Variable(tf.random_normal([50, calc3.shape[1].value])),
-              "biases": tf.Variable(tf.random_normal([50]))}
-
-    dense2 = {"weights": tf.Variable(tf.random_normal([50, dense1["weights"].shape[0].value])),
-              "biases": tf.Variable(tf.random_normal([50]))}
-
-    dense3 = {"weights": tf.Variable(tf.random_normal([5600, dense2["weights"].shape[0].value])),
-              "biases": tf.Variable(tf.random_normal([5600]))}
-
-    m_result1 = tf.tanh(tf.add(tf.matmul(calc3, tf.transpose(dense1["weights"])),tf.reshape(dense1["biases"],[1,50])))
-    m_result2 = tf.tanh(tf.add(tf.matmul(m_result1, tf.transpose(dense2["weights"])),tf.reshape(dense2["biases"],[1,50])))
-    m_result3 = tf.sigmoid(tf.add(tf.matmul(m_result2, tf.transpose(dense3["weights"])),tf.reshape(dense3["biases"],[1,5600])))
-    #m_result3 = tf.reshape(m_result3,[5877,700,8])
-    return {"result3":m_result3,
-            "result2":m_result2,
-            "result1":m_result1,
-            "dense1": dense1,
-            "dense2": dense2,
-            "dense3": dense3}
-
-
-def back_prop(data):
-    lr = tf.Variable(0.15)
-    data = forward_prop(data)
-    res = data["result3"]
-    train_y = tf.reshape(y[:, :, :, 0], res.shape)
-    res = tf.square(tf.subtract(train_y,res))
-    res = tf.divide(res,tf.multiply(2, data["result3"].shape[0]))
-    res = tf.reduce_mean(res,axis=0)
-    #res = tf.reduce_sum(res,axis=1)
-    print("cost: ",res)
-    res = tf.reshape(res,[res.shape[0],1])
-
-back_prop(x)
-"""
