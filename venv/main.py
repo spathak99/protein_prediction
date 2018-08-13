82f29041edc2efcaf2704679a265aee8b7c7fdf3
Shardool Pathak <shardoolpathak@gmail.com>
2018-08-13 14:27:06 -0700

    Added LSTM

--- venv/main.py
+++ venv/main.py
@@ -5,6 +5,7 @@ import numpy as np
 import gzip
 import tensorflow as tf
 import tensorflow.contrib.eager as tfe
+import math
 
 tf.enable_eager_execution()
 
@@ -22,24 +23,57 @@ print(test_label.shape, " ", test_data.shape)
 test_label = tf.reshape(test_label,[272,700,10])
 test_data = tf.reshape(test_data,[272,700,22])
 
-
 #x = tf.placeholder(tf.float32, shape=[None,training_data.shape[1],training_data.shape[2]])
 #y = tf.placeholder(tf.float32, shape=[None,training_labels.shape[1],training_labels.shape[2]])
 
 output_dim=training_labels.shape[2]
 num_nodes = 100
 
-wa = tfe.Variable(tf.random_normal([data.shape[1], num_nodes, num_nodes]))
-ba = tfe.Variable(tf.random_normal([data.shape[1], 1, wa.shape[1].value]))
-wx = tfe.Variable(tf.random_normal([data.shape[1], num_nodes, training_data.shape[2]]))
-bx = tfe.Variable(tf.random_normal([data.shape[1], 1, wx.shape[1].value]))
-wy = tfe.Variable(tf.random_normal([data.shape[1], output_dim, num_nodes]))
-by = tfe.Variable(tf.random_normal([data.shape[1], 1, output_dim]))
+wa = tfe.Variable(tf.random_normal([num_nodes, num_nodes]))
+ba = tfe.Variable(tf.random_normal([1, wa.shape[0].value]))
+wx = tfe.Variable(tf.random_normal([num_nodes, training_data.shape[2]]))
+bx = tfe.Variable(tf.random_normal([1, wx.shape[0].value]))
+wy = tfe.Variable(tf.random_normal([output_dim, num_nodes]))
+by = tfe.Variable(tf.random_normal([1, output_dim]))
+
+#incx = tfe.Variable(tf.random_normal([1, training_data.shape[2]]))
+#inca = tfe.Variable(tf.random_normal([1, num_nodes]))
+#bi = tfe.Variable(tf.random_normal([1]))
+
+fcx = tfe.Variable(tf.random_normal([1, training_data.shape[2]]))
+fca = tfe.Variable(tf.random_normal([1, num_nodes]))
+bf = tfe.Variable(tf.random_normal([1]))
+
+
+wcx = tfe.Variable(tf.random_normal([num_nodes, training_data.shape[2]]))
+wca = wa = tfe.Variable(tf.random_normal([num_nodes, num_nodes]))
+bc = tfe.Variable(tf.random_normal([1,num_nodes]))
+
+wox = tfe.Variable(tf.random_normal([1, training_data.shape[2]]))
+woa = tfe.Variable(tf.random_normal([1, num_nodes]))
+bo = tfe.Variable(tf.random_normal([1]))
+
+def lstm_cell(data, a, c, first=False):
+    if(first):
+        c = tfe.Variable(tf.zeros([data.shape[0], num_nodes]))
+
+    #in_gate = tf.sigmoid(tf.add(tf.add(tf.matmul(data, tf.transpose(incx)), tf.matmul(a, tf.transpose(inca))), bi))
+    forget_gate = tf.sigmoid(tf.add(tf.add(tf.matmul(data, tf.transpose(fcx)), tf.matmul(a, tf.transpose(fca))), bf))
+    #print(forget_gate)
+    #print("in gate", in_gate, " forget gate", forget_gate)
+
+    nextc = tf.tanh(tf.add(tf.add(tf.matmul(a,tf.transpose(wca)), tf.matmul(data,tf.transpose(wcx))), bc))
+
+    c = tf.add(tf.multiply(forget_gate, c), tf.multiply(tf.subtract(1, forget_gate), nextc))
+
+    out_gate = tf.sigmoid(tf.add(tf.add(tf.matmul(data, tf.transpose(wox)), tf.matmul(a, tf.transpose(woa))), bo))
+
+    return c, out_gate
 
 def train(x, y):
     preds = forward_prop(x)
    # print(preds.shape)
-    print("preds", preds.shape)
+    #print("preds", preds.shape)
     cost = tf.reduce_mean(
         tf.reduce_sum(
             tf.subtract(
@@ -55,42 +89,50 @@ def train(x, y):
     print(cost)
     return cost
 
-def timestep(data, a, stepindex=0, first=False):
+def timestep(data, a, c, first=False):
+    out_gate = 0
     if(first):
         a = tfe.Variable(tf.zeros([data.shape[0], num_nodes]))
+        c, out_gate = lstm_cell(data, a, None, first = True)
+    else:
+        c, out_gate = lstm_cell(data, a, c)
 
+    #a1 = [tf.sigmoid(tf.add(tf.matmul(a, tf.transpose(wa)),ba)),tf.sigmoid(tf.add(tf.matmul(data,tf.transpose(wx)),bx))]
     a1 = [tf.sigmoid(
         tf.add(
-            tf.matmul(a, tf.transpose(wa[stepindex])),
-            ba[stepindex])),
+            tf.matmul(a, tf.transpose(wa)),
+            ba)),
         tf.sigmoid(
             tf.add(
                 tf.matmul(data,
-                          tf.transpose(wx[stepindex])),
-                bx[stepindex]))]
+                          tf.transpose(wx)),
+                bx))]
 
-    conc = tf.add(a1[0],a1[1])
-    #print(conc.shape)
+    conc = (tf.add(a1[0],a1[1]))
+    #conc = tf.add(tf.multiply(out_gate, c), tf.multiply(tf.subtract(1, out_gate), conc))
 
-    yh = tf.nn.softmax(tf.add((tf.matmul(conc, tf.transpose(wy[stepindex]))), by[stepindex]))
+    #print("out gate", out_gate)
+
+    yh = tf.nn.softmax(tf.add((tf.matmul(conc, tf.transpose(wy))), by))
     #print("y HAT: ",yh.shape)
-    return conc, yh
+    return conc, yh, c
 
 
 def forward_prop(data):
     preds = []#tfe.Variable([], dtype=tf.float32) #tfe.Variable(dtype=tf.float32, graph=None)
     first = True
-    curr_a,curr_y = timestep(tf.transpose(data,[1,0,2])[0],None,first=True)
+    curr_a,curr_y,curr_c = timestep(tf.transpose(data,[1,0,2])[0],None, None, first=True)
     preds.append(curr_y)
     #return tf.map_fn(lambda x: timestep(x),tf.transpose(data,[1,0,2])[1:])
     for i in tf.transpose(data,[1,0,2]):
         if first:
             first = False
         else:
-            curr_a,curr_y = timestep(i,curr_a)
+            curr_a,curr_y,curr_c = timestep(i, curr_a, curr_c)
             preds.append(curr_y)
     preds = tf.convert_to_tensor(preds, dtype=tf.float32)
     preds = tf.transpose(preds, [1, 0, 2])
+    #print(preds)
     return preds
 
 
